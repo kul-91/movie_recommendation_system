bee0bdd2e9fca8aa91246667c1c20f9b40aa6467
kul-93 <kuldeepchaturvedi517@gmail.com>
2026-06-21 08:22:07 +0530

    web dependencies are installedd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const axios = require('axios');
+import express from "express";
+import axios from "axios";
+
 const app = express();
 
 app.get('/api/recommendations', async (req, res) => {
